65f96246a78145b5595eaddb387c584d85558aa6
Fagge1 <ahmadshuaibibrahim@.com>
2024-12-05 21:07:49 +0100

    challenge-4

---
+++ QQ.js
@@ -0,0 +1,13 @@
+let cars = ["toyota", "honda", "mazda", "mitsubishi", "tesla", "lexus"];
+cars[6] = "mercedes-benz";
+
+    console.log(cars);
+    console.log(cars[0]);
+    console.log(cars[5]);
+    console.log(cars[6]);
+
+let numofcars = cars.length;
+    console.log(numofcars);
+
+let index = cars.indexOf("lexus");
+    console.log(index);
---
+++ testing.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CARS</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <h1>Car Listining</h1>
+    <p id="demo"></p>
+    <script>
+       let cars = ["toyota", "honda", "mazda", "mitsubishi", "tesla", "lexus"];
+       document.getElementById("demo").innerHTML = cars.toString();
+           console.log(0)
+
+    </script>
+
+</body>
+
+</html>
